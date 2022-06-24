855ef03375fec9c828f42f896b107222b66e40bd
Sam Gammon <sam@dyme.com>
2022-06-23 19:17:52 -0700

    Remove unused imports

--- plugin-build/plugin/src/main/java/dev/elide/buildtools/gradle/plugin/ElideExtension.kt
+++ plugin-build/plugin/src/main/java/dev/elide/buildtools/gradle/plugin/ElideExtension.kt
@@ -1,8 +1,6 @@
 package dev.elide.buildtools.gradle.plugin
 
 import org.gradle.api.Project
-import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.Property
 import javax.inject.Inject
 
 @Suppress("UnnecessaryAbstractClass")
--- plugin-build/plugin/src/test/java/dev/elide/buildtools/gradle/plugin/ElidePluginTest.kt
+++ plugin-build/plugin/src/test/java/dev/elide/buildtools/gradle/plugin/ElidePluginTest.kt
@@ -2,7 +2,6 @@ package dev.elide.buildtools.gradle.plugin
 
 import dev.elide.buildtools.gradle.plugin.tasks.EmbeddedJsBuildTask
 import org.gradle.testfixtures.ProjectBuilder
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import java.io.File
