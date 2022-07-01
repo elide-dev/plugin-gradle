9cbcbd19ad4dd6b6c99a29e7dd1d9e3fc7c764d1
Sam Gammon <sam@dyme.com>
2022-07-01 12:37:30 -0700

    Add task to resolve all deps

--- plugin-build/build.gradle.kts
+++ plugin-build/build.gradle.kts
@@ -63,3 +63,25 @@ tasks.register("clean", Delete::class.java) {
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
+
+if (tasks.findByName("resolveAllDependencies") == null) {
+    tasks.register("resolveAllDependencies") {
+        val npmInstall = tasks.findByName("kotlinNpmInstall")
+        if (npmInstall != null) {
+            dependsOn(npmInstall)
+        }
+        doLast {
+            allprojects {
+                configurations.forEach { c ->
+                    if (c.isCanBeResolved) {
+                        println("Downloading dependencies for '$path' - ${c.name}")
+                        val result = c.incoming.artifactView { lenient(true) }.artifacts
+                        result.failures.forEach {
+                            println("- Ignoring Error: ${it.message}")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
