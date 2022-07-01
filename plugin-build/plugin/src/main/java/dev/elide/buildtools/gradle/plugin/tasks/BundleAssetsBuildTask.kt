95c4bf3009c46b854a9a6d9b0a0715f5eb42dba8
Sam Gammon <sam@dyme.com>
2022-07-01 12:58:14 -0700

    More nitfixes

--- plugin-build/plugin/src/main/java/dev/elide/buildtools/gradle/plugin/tasks/BundleAssetsBuildTask.kt
+++ plugin-build/plugin/src/main/java/dev/elide/buildtools/gradle/plugin/tasks/BundleAssetsBuildTask.kt
@@ -4,7 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.plugins.BasePlugin
 
 /** Task which creates Elide asset specifications for embedding in app JARs. */
-abstract class BundleAssetsBuildTask: DefaultTask() {
+abstract class BundleAssetsBuildTask : DefaultTask() {
     init {
         description = "Configures an application target for use with ESBuild or Webpack"
         group = BasePlugin.BUILD_GROUP
