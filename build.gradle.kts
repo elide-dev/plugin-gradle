1a98ad076b8c26bcb66a87c144d3d99d1834ae8f
Sam Gammon <sam@dyme.com>
2022-07-05 17:55:35 -0700

    Drop ktlint version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,7 +84,6 @@ subprojects {
         outputToConsole.set(true)
         ignoreFailures.set(false)
         enableExperimentalRules.set(true)
-        version.set("0.46.1")
         filter {
             exclude("**/generated/**")
             include("**/kotlin/**")
--- plugin-build/build.gradle.kts
+++ plugin-build/build.gradle.kts
@@ -38,7 +38,6 @@ allprojects {
         outputToConsole.set(true)
         ignoreFailures.set(false)
         enableExperimentalRules.set(true)
-        version.set("0.46.1")
         filter {
             exclude("**/generated/**")
             include("**/kotlin/**")
