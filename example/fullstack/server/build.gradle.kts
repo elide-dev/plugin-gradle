babeaa30c0df3d3f60500c88458b7736c1cfdc84
Sam Gammon <sam@dyme.com>
2022-07-10 20:18:12 -0700

    Feature: Brotli Pre-compression
    This changeset fixes a number of compression bugs introduced in
    the last PR, and also adds support for Brotli asset compression
    at build time.
    
    Changes enclosed:
    - Fix use of zlib vs. gzip (answer: support both)
    - Add optimized Gzip implementation and use it
    - Add support for Brotli compression (where applicable)

--- example/fullstack/server/build.gradle.kts
+++ example/fullstack/server/build.gradle.kts
@@ -24,7 +24,7 @@ elide {
         }
         assets {
             bundler {
-                format(ManifestFormat.TEXT)
+                format(ManifestFormat.BINARY)
                 digestAlgorithm(HashAlgorithm.SHA256)
 
                 tagGenerator {
