332d46ac5e760d7395e79e3c6eaf380fd7ac0c32
Sam Gammon <sam@dyme.com>
2022-11-28 17:01:08 -0800

    chore: remove duplicate repos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,6 @@ plugins {
 
 dependencyResolutionManagement {
     repositories {
-        google()
-        maven("https://maven-central.storage-download.googleapis.com/maven2/")
         mavenCentral()
         maven("https://plugins.gradle.org/m2/")
         maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
