5e9884cc6a8d90343b45da52339721354f9ed14a
Sam Gammon <sam@elide.ventures>
2023-07-22 15:57:59 -0700

    fix: plugin repository order

--- plugin-build/settings.gradle.kts
+++ plugin-build/settings.gradle.kts
@@ -4,8 +4,8 @@ import build.less.plugin.settings.buildless
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.st/")
         maven("https://gradle.pkg.st/")
+        maven("https://maven.pkg.st/")
         maven("https://elide-snapshots.storage-download.googleapis.com/repository/v3/")
     }
 }
